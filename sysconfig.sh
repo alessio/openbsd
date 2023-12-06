e3e994a400040ea528b2308290da875c7bb809da
Alessio Treglia <alessio@jur.io>
2023-12-06 21:38:26 +0100

    new config

--- sysconfig.sh
+++ sysconfig.sh
@@ -1,37 +1,5 @@
-touch /etc/sysctl.conf
-cat >>/etc/sysctl.conf <<EOF
-# shared memory limits (chrome needs a ton)
-kern.shminfo.shmall=3145728
-kern.shminfo.shmmax=2147483647
-kern.shminfo.shmmni=1024
-
-# semaphores
-kern.shminfo.shmseg=1024
-kern.seminfo.semmns=4096
-kern.seminfo.semmni=1024
-
-kern.maxproc=32768
-kern.maxfiles=65535
-kern.bufcachepercent=90
-kern.maxvnodes=262144
-kern.somaxconn=2048
-EOF
-
-touch /etc/login.conf
-cat >>/etc/login.conf <<EOF
-
-staff:\
-  :datasize-cur=infinity:\
-  :datasize-max=infinity:\
-  :maxproc-cur=512:\
-  :maxproc-max=1024:\
-  :openfiles-cur=102400:\
-  :openfiles-max=102400:\
-  :stacksize-cur=32M:\
-  :ignorenologin:\
-  :requirehome@:\
-  :tc=default:
-EOF
+cp sysctl.conf /etc
+cp login.conf.d/* /etc/login.comf.d/
 
 touch /etc/doas.conf
 echo 'permit nopass keepenv :wheel' >> /etc/doas.conf
